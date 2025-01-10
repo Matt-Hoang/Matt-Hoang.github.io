e94d65e5ba3353369509a07b7078b7ae61fe19dd
Matthew Hoang <Matthew.Hoang@pacificlife.com>
2025-01-10 15:23:18 -0800

    mobile view updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa'; // Import icons
+import { FaLinkedin, FaGithub, FaInstagram, FaEnvelope } from 'react-icons/fa'; // Import icons
 import './App.css';
 import Experience from './Experience';
 import Projects from './Projects';
@@ -9,6 +9,7 @@ import ContactForm from './ContactForm';
 
 const App = () => {
   const [activeSection, setActiveSection] = useState('About');
+  const [isChecked, setIsChecked] = useState(false);
 
   const sectionRefs = {
     About: useRef(null),
@@ -50,12 +51,20 @@ const App = () => {
 
   const handleScrollToSection = (section) => {
     sectionRefs[section].current.scrollIntoView({ behavior: 'smooth' });
+
+    setIsChecked((prev) => !prev);
   };
 
   return (
     <div className="layout">
+      <div className='mobile-nav-banner'>
+        <h1>MATT HOANG</h1>
+      </div>
+      <label className = "hamburger-menu">
+        <input type="checkbox" checked={isChecked} onChange={() => setIsChecked((prev) => !prev)}></input>
+      </label>
       <nav className="nav">
-        <img src={require('./images/headshot.JPG')} alt="headshot" className="headshot-img" />
+        <img src={require('./images/headshot.JPG')} alt="headshot" className="headshot-img-desktop" />
         <ul>
           <li className={activeSection === 'About' ? 'active' : ''} onClick={() => handleScrollToSection('About')}>ABOUT</li>
           <li className={activeSection === 'Experience' ? 'active' : ''} onClick={() => handleScrollToSection('Experience')}>EXPERIENCE</li>
@@ -70,10 +79,13 @@ const App = () => {
             <span className="first-name">MATT </span>
             <span className="last-name">HOANG</span>
           </h1>
-          <h5>Garden Grove, CA • (714) 618-2844 • MattHoang19@gmail.com</h5>
-          <p>Hi, I’m Matt Hoang! I graduated from California State University Long Beach (CSULB) with a bachelor’s degree in computer science. My primary focus is software development, where I’ve built a variety of web and desktop applications. I’m also keen on expanding my expertise into data and analytics as well as IT infrastructure and security.</p>
-          <p>Outside of my professional pursuits, I cherish time with my dog Lulu and my family. I’m passionate about staying active, whether it’s indoor bouldering and lifting during the week or exploring the great outdoors with hiking and backpacking whenever I can.</p>
+          <img src={require('./images/headshot.JPG')} alt="headshot" className="headshot-img-mobile" />
+          <p>Hey, I’m Matt Hoang! I graduated from California State University Long Beach (CSULB) with a bachelor’s degree in computer science. My primary focus is software development, where I’ve worked on building various web and desktop applications. I’m also actively expanding my skill set in areas that complement and enhance my software engineering expertise, such as cloud computing, DevOps, and automation.</p>
+          <p>Outside of my professional pursuits, I cherish spending time with my family and my dog, Lulu. I’m passionate about staying active, whether it’s indoor bouldering, weightlifting, or exploring the great outdoors through hiking and backpacking.</p>
           <div className="social-icons">
+            <a href="mailto:matthoang19@gmail.com" target="_blank" rel="noopener noreferrer">
+              <FaEnvelope className="icon" />
+            </a>
             <a href="https://www.linkedin.com/in/matthewhhoang" target="_blank" rel="noopener noreferrer">
               <FaLinkedin className="icon" />
             </a>
@@ -83,6 +95,7 @@ const App = () => {
             <a href="https://www.instagram.com/matt_hoang/" target="_blank" rel="noopener noreferrer">
               <FaInstagram className="icon" />
             </a>
+
           </div>
         </section>
         <section id="Experience" ref={sectionRefs.Experience} className="content-section">
@@ -94,7 +107,7 @@ const App = () => {
             }}
             positions={[
               {
-                position: "Information Technolgy Leadership Program",
+                position: "IT Leadership Program",
                 dates: "July 2024 - Present",
                 description: [
                   { type: "level-one", content: "Participating in a rotational program consisting of four 6-month rotations though various technology departments at Pacific Life to build strong technical and analytical skills." },
@@ -141,7 +154,7 @@ const App = () => {
             }}
             positions={[
               {
-                position: "Cloud Engineering/Computer Science Intern",
+                position: "Cloud Engineering & Computer Science Intern",
                 dates: "May 2022 - March 2023",
                 description: [
                   { type: "level-one", content: "Self-taught MS Azure, Apache Kafka, LabVIEW, and other tools for designing and testing cloud architecture for low-latency computations." },
@@ -160,38 +173,38 @@ const App = () => {
           <div>
             <Projects
               title="Personal Website"
-              skills="React"
+              skills="React | UI/UX"
               image={require('./images/projects/Personal-Website.png')}
-              description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga deleniti labore excepturi magni porro tempore iure ipsa quidem perspiciatis aliquid laudantium eum id, facere vitae assumenda consequuntur quae! Minus, natus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga deleniti labore excepturi magni porro tempore iure ipsa quidem perspiciatis aliquid laudantium eum id, facere vitae assumenda consequuntur quae! Minus, natus!"
+              description="Rebuilt my personal website to replace an older version, using the project as an opportunity to learn React. I was able to create a more dynamic and responsive experience, while refining the visual design to provide a more polished and user-friendly interface. This project served as both a practical learning experience and a way to showcase my growth in web development."
               links = {[
                 {text: "Figma", url: ""},
-                {text: "GitHub", url: ""}
+                {text: "GitHub", url: "https://github.com/Matt-Hoang/Personal-Website"}
               ]}
             />
             <Projects
               title="Census Helper Tool"
               skills="Python | MVC | JFrog Artifactory | SonarQube | Pipelines"
               image={require('./images/projects/Census-Helper.png')}
-              description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga deleniti labore excepturi magni porro tempore iure ipsa quidem perspiciatis aliquid laudantium eum id, facere vitae assumenda consequuntur quae! Minus, natus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga deleniti labore excepturi magni porro tempore iure ipsa quidem perspiciatis aliquid laudantium eum id, facere vitae assumenda consequuntur quae! Minus, natus!"
+              description="Refactored an internal tool from a monolithic approach to the MVC pattern, improving the separation of concerns. Enhanced and streamlined the validation logic to reduce redundancy, making the code more reusable with generalized functions. Also implemented an automated CI/CD pipeline, integrated JFrog Artifactory, and set up SonarQube for code quality scanning."
             />
             <Projects
               title="QuackQuackGo"
-              skills="HTML | CSS | JS | Firebase | APIs"
+              skills="HTML | CSS | JS | Firebase | APIs | UI/UX"
               image={require('./images/projects/QuackQuackGo.jpeg')}
-              description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga deleniti labore excepturi magni porro tempore iure ipsa quidem perspiciatis aliquid laudantium eum id, facere vitae assumenda consequuntur quae! Minus, natus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga deleniti labore excepturi magni porro tempore iure ipsa quidem perspiciatis aliquid laudantium eum id, facere vitae assumenda consequuntur quae! Minus, natus!"
+              description="Designed, developed, and documented an itinerary web application with a team of five members. Utilized HTML, CSS, and JavaScript to create a responsive, intuitive, and aesthetically pleasing user interface. Organized the Firebase database to ensure integrity, reliability, and efficient data retrieval from Firebase APIs."
               links = {[
-                {text: "Figma", url: ""},
-                {text: "GitHub", url: ""},
-                {text: "Youtube", url: ""}
+                {text: "Figma", url: "https://www.figma.com/design/3rHTXa60UDVTXoWFrnrvG3/Prototypes?node-id=0-1&t=GOG3eI2iBbSiOwaE-1"},
+                {text: "GitHub", url: "https://github.com/Matt-Hoang/QuackQuackGo"},
+                {text: "Youtube", url: "https://www.youtube.com/watch?v=eTzmMGJANaE"}
               ]}
             />
             <Projects
               title="Dungeon Master"
               skills="Java - Design Patterns"
               image={require('./images/projects/Dungeon-Master.jpg')}
-              description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga deleniti labore excepturi magni porro tempore iure ipsa quidem perspiciatis aliquid laudantium eum id, facere vitae assumenda consequuntur quae! Minus, natus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga deleniti labore excepturi magni porro tempore iure ipsa quidem perspiciatis aliquid laudantium eum id, facere vitae assumenda consequuntur quae! Minus, natus!"
+              description="Developed a console-based game in Java that allowed users to fight monsters on a 5x5 map, advance to the subsequent map, and shop for items. This was created by utilizing design patterns such as inheritance and polymorphism to structure methods and objects, with an interactive menu for user actions based on the situation."
               links = {[
-                {text: "GitHub", url: ""},
+                {text: "GitHub", url: "https://github.com/Matt-Hoang/DungeonMaster"},
                 {text: "Replit", url: ""},
                 {text: "Youtube", url: ""}
               ]}
@@ -201,7 +214,7 @@ const App = () => {
         <section id="Skills" ref={sectionRefs.Skills} className="content-section">
           <h1>SKILLS</h1>
           <TechnicalSkills
-            title="Programming and Development"
+            title="Programming & Development"
             subsections={[
               { 
                 title: 'Programming Languages', 
@@ -210,23 +223,35 @@ const App = () => {
                   {name: 'Java', logo: require('./images/skills/java-logo.png')},
                   {name: 'C++', logo: require('./images/skills/cpp-logo.png')},
                   {name: 'JavaScript', logo: require('./images/skills/js-logo.png')},
-                  {name: 'HTML', logo: require('./images/skills/html-logo.png')},
-                  {name: 'CSS', logo: require('./images/skills/css-logo.png')},
-                  {name: 'SQL', logo: require('./images/skills/sql-icon.png')},
                 ] 
               },
               { 
-                title: 'Web Development', 
+                title: 'Markup & Style Languages', 
                 skills: [
+                  {name: 'HTML', logo: require('./images/skills/html-logo.png')},
+                  {name: 'CSS', logo: require('./images/skills/css-logo.png')},
                   {name: 'React.js', logo: require('./images/skills/react-icon.png')},
-                  {name: 'Node.js', logo: require('./images/skills/node-js-icon.png')},
                   {name: 'Angular', logo: require('./images/skills/angular-icon.png')},
+                ] 
+              },
+              { 
+                title: 'Backend', 
+                skills: [
+                  {name: 'Node.js', logo: require('./images/skills/node-js-icon.png')},
+                ] 
+              },
+              { 
+                title: 'Databases', 
+                skills: [
+                  {name: 'SQL', logo: require('./images/skills/sql-icon.png')},
+                  {name: 'PostgresSQL', logo: require('./images/skills/postgresql-icon.png')},
+                  {name: 'Firebase', logo: require('./images/skills/firebase-icon.png')},
                 ] 
               },
             ]}
           />
           <TechnicalSkills
-            title="Systems and Infrastructure"
+            title="Systems & Infrastructure"
             subsections={[
               { 
                 title: 'DevOps and Cloud', 
@@ -241,17 +266,10 @@ const App = () => {
                   {name: 'Linux', logo: require('./images/skills/linux-icon.png')},
                 ] 
               },
-              { 
-                title: 'Databases', 
-                skills: [
-                  {name: 'PostgresSQL', logo: require('./images/skills/postgresql-icon.png')},
-                  {name: 'Firebase', logo: require('./images/skills/firebase-icon.png')},
-                ] 
-              },
             ]}
           />
           <TechnicalSkills
-            title="Tools and Methodologies"
+            title="Tools & Methodologies"
             subsections={[
               { 
                 title: 'Version Control', 
@@ -261,19 +279,37 @@ const App = () => {
                 ] 
               },
               { 
-                title: 'Software Development Methodologies', 
+                title: 'API Testing & Development', 
                 skills: [
-                  {name: 'Agile', logo: ''},
-                  {name: 'Scrum', logo: ''},
-                  {name: 'Waterfall', logo: ''},
+                  {name: 'Postman', logo: require('./images/skills/postman-icon.png')},
+                  {name: 'Swagger', logo: require('./images/skills/swagger-icon.png')},
+                  {name: 'MuleSoft Anypoint Platform', logo: require('./images/skills/mulesoft-icon.png')},
                 ] 
               },
               { 
-                title: 'Other Tools', 
+                title: 'IDE (Integrated Development Environment)', 
                 skills: [
                   {name: 'Visual Studio Code', logo: require('./images/skills/visual-studio-code-icon.png')},
-                  {name: 'Postman', logo: require('./images/skills/postman-icon.png')},
+                  {name: 'Visual Studio', logo: require('./images/skills/visual-studio-icon.png')},
+                  {name: 'MuleSoft Anypoint Studio', logo: require('./images/skills/mulesoft-icon.png')},
+
+                ] 
+              },
+              { 
+                title: 'Design Tools', 
+                skills: [
+                  {name: 'Canva', logo: require('./images/skills/canva-icon.png')},
                   {name: 'Figma', logo: require('./images/skills/figma-icon.png')},
+                  {name: 'LucidChart', logo: require('./images/skills/lucidchart-icon.png')},
+                ] 
+              },
+              { 
+                title: 'Methodologies', 
+                skills: [
+                  {name: 'Agile', logo: ''},
+                  {name: 'Scrum', logo: ''},
+                  {name: 'Kanban', logo: ''},
+                  {name: 'Waterfall', logo: ''},
                 ] 
               },
             ]}
